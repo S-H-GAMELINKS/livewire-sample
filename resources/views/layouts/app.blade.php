00526f29c287b87e4f515e2cfa11ccfa5184c6b7
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-17 17:15:41 +0900

    add layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,12 @@
 <html>
     <head>
         <title>App Name - @yield('title')</title>
+        @livewireStyles
     </head>
     <body>
         <div class="container">
             @yield('content')
         </div>
+        @livewireScripts
     </body>
 </html>
